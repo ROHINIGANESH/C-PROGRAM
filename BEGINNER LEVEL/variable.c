0427941fc919672c5cad7102463712536be3788b
ROHINIGANESH <33023274+ROHINIGANESH@users.noreply.github.com>
2017-12-19 05:44:40 +0000

    Create variable.c

---
+++ BEGINNER LEVEL/variable.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int RSO=18426;
+int OM=463;
+float BAO=44.83;
+char name[30]="Sachin Ramesh Tendulkar";
+char nick[20]="Little Master";
+printf("%s\n",name);
+printf("%s\n",nick);
+printf("%d\n",RSO);
+printf("%f\n",BAO);
+printf("%d\n",OM);
+}
